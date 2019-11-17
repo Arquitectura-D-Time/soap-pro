97663542b1c66a7ededbe8955e36a81f85ddab46
Rafael Cruz <ercruzr@unal.edu.co>
2019-11-17 14:30:31 -0500

    archivo wsdl

--- app.js
+++ app.js
@@ -1,9 +1,71 @@
+/*jslint node: true */
+"use strict";
+
 const express 	= require("express"); 
-const app 	= express();
-const answer = require('./src/expose')
+//const answer = require('./src/expose')
+var soap = require('soap');
+var fs = require('fs');
+var xml = fs.readFileSync('tutorias.wsdl', 'utf8');
+var app 	= express();
+var fetch = require("node-fetch");
+var port = 3007;
 
-app.use('/', answer.tuto);
+// app.use('/', answer.tuto);
 
-var server = app.listen(3007, function () {
-    console.log('Server is running..'); 
+// funcion que divide el json
+function splitter_function(args) {
+    console.log('splitter_function');
+    var splitter = args.splitter;
+    var splitted_msg = args.message.split(splitter);
+    var result = [];
+    for(var i=0; i<splitted_msg.length; i++){
+      result.push(splitted_msg[i]);
+    }
+    return {
+        result: result
+        }
+}
+
+function getTutoriasById(args){
+    console.log('Muestra tutoria por id');
+    var id = args.id;
+    var response;
+    return fetch("http://146.148.107.218:5002/tutorias/"+id).then(res => res.json())
+    .then(data => {
+      console.log(data);
+      response = data;
+      return {
+        id: data.id,
+        materia:data.materia,
+        descripcion:data.descripcion,
+        cupos:data.cupos,
+        idtutor:data.idtutor,
+        idtoken:data.idtoken
+        }
+    })
+  }
+
+// servicio
+var serviceObject = {
+    MessageSplitterService: {
+          MessageSplitterServiceSoapPort: {
+              MessageSplitter: splitter_function,
+              TutoriaByID: getTutoriasById
+          },
+          MessageSplitterServiceSoap12Port: {
+              MessageSplitter: splitter_function,
+              TutoriaByID: getTutoriasById
+          }
+    }
+  };
+
+app.get('/', function (req, res) {
+  res.send('Bienvenido SOAP-Tutorias');
+})
+
+app.listen(port, function () {
+  console.log(`Servidor SOAP corriendo en el puerto ${port}.`); 
+  var wsdl_path = "/wsdl";
+  soap.listen(app, wsdl_path, serviceObject, xml);
+  console.log("WSDL en http://localhost:" + port + wsdl_path +"?wsdl");
 });
